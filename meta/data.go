93f88682f8b3e93282d8a7bcd92e2f14a8e6de33
zhexuany <zhexuany@gmail.com>
2016-11-22 12:40:46 +0000

    finished client.go

--- meta/data.go
+++ meta/data.go
@@ -8,7 +8,7 @@ import (
 	"github.com/gogo/protobuf/proto"
 	"github.com/influxdata/influxdb"
 	"github.com/influxdata/influxdb/influxql"
-	"github.com/influxdata/influxdb/services/meta/internal"
+	"github.com/zhexuany/influxdb-cluster/meta/internal"
 )
 
 //go:generate protoc --gogo_out=. internal/meta.proto
