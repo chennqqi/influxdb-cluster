bdfe576f1aad83c5ee51394515be3471df16f101
zhexuany <zhexuany@gmail.com>
2016-12-20 11:18:25 +0000

    added two more package cluster and tlv

---
+++ cluster/service.go
@@ -0,0 +1,113 @@
+package cluster
+
+type Service struct {
+}
+
+func (s *Service) Open() {
+
+}
+func (s *Service) SetLogOutput() {
+
+}
+func (s *Service) serve() {
+
+}
+func (s *Service) Close() {
+
+}
+func (s *Service) Statistics() {
+
+}
+func (s *Service) handleConn() {
+
+}
+func (s *Service) executeStatement() {
+
+}
+func (s *Service) processWriteShardRequest() {
+
+}
+func (s *Service) writeShardResponse() {
+
+}
+func readUntilEOF() {
+
+}
+func (s *Service) processCreateIteratorRequest() {
+
+}
+func (s *Service) processFieldDimensionsRequest() {
+
+}
+func (s *Service) processJoinClusterRequest() {
+
+}
+func (s *Service) writeJoinClusterResponse() {
+
+}
+func (s *Service) importMetaData() {
+
+}
+func (s *Service) processLeaveClusterRequest() {
+
+}
+func (s *Service) writeLeaveClusterResponse() {
+
+}
+func (s *Service) processCreateShardSnapshotRequest() {
+
+}
+func (s *Service) processDeleteShardSnapshotRequest() {
+
+}
+func ReadTLV() {
+
+}
+func (s *Service) processExpandSourcesRequest() {
+
+}
+func (s *Service) processDownloadShardSnapshotRequest() {
+
+}
+func (s *Service) processCopyShardRequest() {
+
+}
+func (s *Service) processRemoveShardRequest() {
+
+}
+func (s *Service) shardSnapshot() {
+
+}
+func (s *Service) deleteSnapshot() {
+
+}
+func (s *Service) downloadShardSnapshot() {
+
+}
+func (s *Service) processShardStatusRequest() {
+
+}
+func (s *Service) processCopyShardStatusRequest() {
+
+}
+func (s *Service) processKillCopyShardRequest() {
+
+}
+func (s *Service) processShowQueriesRequest() {
+
+}
+func (s *Service) processKillQueryRequest() {
+
+}
+func (s *Service) processRestoreShard() {
+
+}
+func (s *Service) processShowMeasurements() {
+
+}
+func (s *Service) processShowTagValues() {
+
+}
+func (bfc *BufferedWriteCloser) Close() {
+
+}
---
+++ tlv/tlv.go
@@ -0,0 +1,46 @@
+package tlv
+
+import ()
+
+func ReadType(r io.Reader) {
+	// io.ReadFull(r, buf)
+}
+
+func ReadLV() {
+
+}
+
+func WriteTLV() {
+
+}
+
+func WriteType() {
+
+}
+
+func WriteTLV() {
+
+}
+
+func EncodeTLV() {
+	//WriteTLV
+	//EncodeLV
+	//
+}
+
+func EncodeLV() {
+	//writeLv
+	//
+
+}
+
+func DecodeTLV() {
+
+}
+
+func DecodeLV() {
+
+}
+
+func init() {
+}
