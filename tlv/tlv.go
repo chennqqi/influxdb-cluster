32022f6af9070a0b88aeaca5f8102686631256e7
zhexuany <zhexuany@gmail.com>
2016-12-21 17:38:40 +0800

    finished tlv

--- tlv/tlv.go
+++ tlv/tlv.go
@@ -1,46 +1,137 @@
 package tlv
 
-import ()
+import (
+	"encoding"
+	"encoding/binary"
+	"fmt"
+	"io"
+)
 
-func ReadType(r io.Reader) {
-	// io.ReadFull(r, buf)
+// MaxMessageSize defines how large a message can be before we reject it
+const MaxMessageSize = 1024 * 1024 * 1024 // 1GB
+
+// ReadTLV reads a type-length-value record from r.
+func ReadTLV(r io.Reader) (byte, []byte, error) {
+	typ, err := ReadType(r)
+	if err != nil {
+		return 0, nil, err
+	}
+
+	buf, err := ReadLV(r)
+	if err != nil {
+		return 0, nil, err
+	}
+	return typ, buf, err
 }
 
-func ReadLV() {
-
+// ReadType reads the type from a TLV record.
+func ReadType(r io.Reader) (byte, error) {
+	var typ [1]byte
+	if _, err := io.ReadFull(r, typ[:]); err != nil {
+		return 0, fmt.Errorf("read message type: %s", err)
+	}
+	return typ[0], nil
 }
 
-func WriteTLV() {
+// ReadLV reads the length-value from a TLV record.
+func ReadLV(r io.Reader) ([]byte, error) {
+	// Read the size of the message.
+	var sz int64
+	if err := binary.Read(r, binary.BigEndian, &sz); err != nil {
+		return nil, fmt.Errorf("read message size: %s", err)
+	}
 
+	if sz >= MaxMessageSize {
+		return nil, fmt.Errorf("max message size of %d exceeded: %d", MaxMessageSize, sz)
+	}
+
+	// Read the value.
+	buf := make([]byte, sz)
+	if _, err := io.ReadFull(r, buf); err != nil {
+		return nil, fmt.Errorf("read message value: %s", err)
+	}
+
+	return buf, nil
 }
 
-func WriteType() {
-
+// WriteTLV writes a type-length-value record to w.
+func WriteTLV(w io.Writer, typ byte, buf []byte) error {
+	if err := WriteType(w, typ); err != nil {
+		return err
+	}
+	if err := WriteLV(w, buf); err != nil {
+		return err
+	}
+	return nil
 }
 
-func WriteTLV() {
-
+// WriteType writes the type in a TLV record to w.
+func WriteType(w io.Writer, typ byte) error {
+	if _, err := w.Write([]byte{typ}); err != nil {
+		return fmt.Errorf("write message type: %s", err)
+	}
+	return nil
 }
 
-func EncodeTLV() {
-	//WriteTLV
-	//EncodeLV
-	//
+// WriteLV writes the length-value in a TLV record to w.
+func WriteLV(w io.Writer, buf []byte) error {
+	// Write the size of the message.
+	if err := binary.Write(w, binary.BigEndian, int64(len(buf))); err != nil {
+		return fmt.Errorf("write message size: %s", err)
+	}
+
+	// Write the value.
+	if _, err := w.Write(buf); err != nil {
+		return fmt.Errorf("write message value: %s", err)
+	}
+	return nil
 }
 
-func EncodeLV() {
-	//writeLv
-	//
-
+// EncodeTLV encodes v to a binary format and writes the record-length-value record to w.
+func EncodeTLV(w io.Writer, typ byte, v encoding.BinaryMarshaler) error {
+	if err := WriteType(w, typ); err != nil {
+		return err
+	}
+	if err := EncodeLV(w, v); err != nil {
+		return err
+	}
+	return nil
 }
 
-func DecodeTLV() {
+// EncodeLV encodes v to a binary format and writes the length-value record to w.
+func EncodeLV(w io.Writer, v encoding.BinaryMarshaler) error {
+	buf, err := v.MarshalBinary()
+	if err != nil {
+		return err
+	}
 
+	if err := WriteLV(w, buf); err != nil {
+		return err
+	}
+	return nil
 }
 
-func DecodeLV() {
-
+// DecodeTLV reads the type-length-value record from r and unmarshals it into v.
+func DecodeTLV(r io.Reader, v encoding.BinaryUnmarshaler) (typ byte, err error) {
+	typ, err = ReadType(r)
+	if err != nil {
+		return 0, err
+	}
+	if err := DecodeLV(r, v); err != nil {
+		return 0, err
+	}
+	return typ, nil
 }
 
-func init() {
+// DecodeLV reads the length-value record from r and unmarshals it into v.
+func DecodeLV(r io.Reader, v encoding.BinaryUnmarshaler) error {
+	buf, err := ReadLV(r)
+	if err != nil {
+		return err
+	}
+
+	if err := v.UnmarshalBinary(buf); err != nil {
+		return err
+	}
+	return nil
 }
